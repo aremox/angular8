dd4fec39505177073535742ba0c39afee75eb2da
aremox <arenasmorante@gmail.com>
2020-04-29 20:24:17 +0200

    b

--- src/app/Components/lista-correos/lista-correos.component.ts
+++ src/app/Components/lista-correos/lista-correos.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaCorreosComponent implements OnInit {
 
-  constructor() { }
+  correos: any[];
+
+  constructor() {
+    const correo1 = {
+      titulo: "Titulo del 1",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email`,
+      emisor: 'correoEmisor1@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: true,
+    };
+    const correo2 = {
+      titulo: "Titulo del 2",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor2@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    };
+    this.correos = [];
+    this.correos.push(correo1);
+    this.correos.push(correo2);
+   }
 
   ngOnInit(): void {
   }
